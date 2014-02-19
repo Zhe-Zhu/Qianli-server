5638c839d91575db34a3950b9bf4755d412b2755
Zhe Zhu <cainholic@gmail.com>
2014-02-19 18:35:36 +0800

    bug

--- captcha/urls.py
+++ captcha/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
                         # url(r'(?P<pk>[0-9]+)/$',
                         #     views.GetPicture.as_view()),    
                         url(r'sendcaptcha/$', views.generateCaptcha.as_view()),
-                        url(r'testWhetherRecieve/$', views.testWhetherRecieve.as_view())，
+                        url(r'testWhetherRecieve/$', views.testWhetherRecieve.as_view()),
                         url(r'sendcaptchabyvoice/$', views.sendCaptchaByVoice.as_view())
                        )
 
